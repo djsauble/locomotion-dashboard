a0fc41a20016662bb497a835b067694a5a65a0fb
Daniel Sauble <djsauble@gmail.com>
2016-06-07 16:06:31 -0700

    Refactor the data logic into a separate file

--- app.js
+++ app.js
@@ -1,114 +1,17 @@
-$(function(exports) {
-  // Globals
-  //var user     = 'ckinamistiongedenterattl';
-  //var password = '9047d84b808fe51b65ff94624f80acf8f55fcd63';
-  var ns = "Forrest",
-      database = 'be7b25ca3682ef8a15682f791c6110648152d7e4',
-      remoteDB = new PouchDB('http://127.0.0.1:5984/' + database),
-      localDB = new PouchDB(database),
-      Run = Backbone.Model.extend({
-        defaults: function() {
-          return {
-            _id: null,
-            _rev: null,
-            timestamp: null,
-            created_by: null,
-            distance: null
-          }
-        }
-      }),
-      Runs = Backbone.Collection.extend({
-        model: Run,
-        pouch: {
-          options: {
-            query: {
-              include_docs: true,
-              fun: {
-                map: function(doc, emit) {
-                  emit(doc.timestamp, null);
-                }
-              }
-            }
-          }
-        },
-        parse: function(result) {
-          return _.pluck(result.rows, 'doc').map(function(d) {
-            d.timestamp = new Date(d.timestamp);
-            return d;
-          });
-        }
-      });
-
-  // Rework the default syncing behavior for compatibility with PouchDB 
-  Backbone.sync = BackbonePouch.sync({
-    db: localDB,
-    fetch: 'query',
-    options: {
-      query: {
-        include_docs: true
-      }
+$(function() {
+  // Events
+  Forrest.runs.on({
+    "sync": function(collection) {
+      console.log("App is loaded with " + collection.length + " records");
+    },
+    "processed": function(collection, count) {
+      console.log(count + " missing attributes calculated");
     }
   });
-  Backbone.Model.prototype.idAttribute = '_id';
 
-  // Populate the local database with data if empty
-  localDB.allDocs().then(function(results) {
-    if (results.total_rows == 0) {
-      return PouchDB.replicate(remoteDB, localDB);
-    }
-    return new Promise(function(resolve) { resolve(); });
-  }).then(function() {
-    init();
+  // Kick the app
+  Forrest.init({
+    host: 'http://127.0.0.1:5984',
+    db: 'be7b25ca3682ef8a15682f791c6110648152d7e4'
   });
-
-  // Export api to the namespace
-  exports[ns] = {
-    Run: Run,
-    Runs: Runs,
-    db: localDB,
-    runs: null // This will be instantiated once the data has loaded
-  }
-
-  // Call this once data is loaded into the app
-  function init() {
-    runs = new Runs;
-
-    runs.fetch().then(function() {
-      console.log("App is loaded with " + runs.length + " records");
-      var missingDistance = [];
-      runs.each(function(r) {
-        if (!r.get('distance')) {
-          console.log("Missing distance! " + r.get('timestamp'));
-          missingDistance.push({
-            id: r.get('_id'),
-            rev: r.get('_rev')
-          });
-        }
-      });
-      if (missingDistance.length > 0) {
-        localDB.bulkGet({docs: missingDistance, attachments: true}).then(function(results) {
-          var docs = results.results.map(function(r) {
-            return r.docs[0].ok;
-          });
-          docs.forEach(function(d) {
-            var data = getRun(d),
-                filtered = defaultFilter(data),
-                coords = getCoordinates(filtered),
-                model = runs.get(d._id);
-
-            model.set('distance', computeDistance(coords));
-            model.save();
-          });
-          localDB.replicate.to(remoteDB).on('complete', function() {
-            console.log("Yay, we're done!");
-          }).on('error', function(err) {
-            console.log(err);
-          });
-        });
-      }
-
-      // Export run collection to the namespace
-      exports[ns].runs = runs;
-    });
-  }
-}(typeof exports === 'undefined' ? window : exports));
+});
---
+++ data.js
@@ -0,0 +1,117 @@
+(function(exports) {
+  var ns = "Forrest",
+      Run = Backbone.Model.extend({
+        defaults: function() {
+          return {
+            _id: null,
+            _rev: null,
+            timestamp: null,
+            created_by: null,
+            distance: null
+          }
+        }
+      }),
+      Runs = Backbone.Collection.extend({
+        model: Run,
+        pouch: {
+          options: {
+            query: {
+              include_docs: true,
+              fun: {
+                map: function(doc, emit) {
+                  emit(doc.timestamp, null);
+                }
+              }
+            }
+          }
+        },
+        parse: function(result) {
+          return _.pluck(result.rows, 'doc').map(function(d) {
+            d.timestamp = new Date(d.timestamp);
+            return d;
+          });
+        },
+        save: function(options) {
+          return Backbone.sync('create', this, options);
+        },
+        process: function() {
+          var me = this,
+              missingDistance = [],
+              done = new Promise(function(resolve) {
+                me.each(function(r) {
+                  if (!r.get('distance')) {
+                    missingDistance.push({
+                      id: r.get('_id'),
+                      rev: r.get('_rev')
+                    });
+                  }
+                });
+
+                if (missingDistance.length > 0) {
+                  localDB.bulkGet({docs: missingDistance, attachments: true}).then(function(results) {
+                    var docs = results.results.map(function(r) {
+                      return r.docs[0].ok;
+                    });
+                    docs.forEach(function(d) {
+                      var data = getRun(d),
+                          filtered = defaultFilter(data),
+                          coords = getCoordinates(filtered),
+                          model = me.get(d._id);
+
+                      model.save('distance', computeDistance(coords));
+                    });
+                    localDB.replicate.to(remoteDB).on('complete', function() {
+                      resolve(missingDistance.length);
+                    });
+                  });
+                }
+                else {
+                  resolve(0);
+                }
+              });
+
+          done.then(function(count) {
+            me.trigger("processed", me, count);
+          });
+        }
+      });
+
+  // Export run collection to the namespace
+  exports[ns] = {
+    runs: new Runs,
+    init: init
+  }
+
+  // Call this once data is loaded into the app
+  function init(options) {
+    var runs = exports[ns].runs,
+        remoteDB = new PouchDB(options.host + '/' + options.db),
+        localDB = new PouchDB(options.db);
+
+    // Rework the default syncing behavior for compatibility with PouchDB 
+    Backbone.sync = BackbonePouch.sync({
+      db: localDB,
+      fetch: 'query',
+      options: {
+        query: {
+          include_docs: true
+        }
+      }
+    });
+    Backbone.Model.prototype.idAttribute = '_id';
+
+    localDB.allDocs().then(function(results) {
+      // Populate the local database with data if empty
+      if (results.total_rows == 0) {
+        return PouchDB.replicate(remoteDB, localDB);
+      }
+      return new Promise(function(resolve) { resolve(); });
+    }).then(function() {
+      // Fetch and parse data from PouchDB
+      return runs.fetch();
+    }).then(function() {
+      // Perform any necessary post-processing
+      runs.process();
+    });
+  }
+}(typeof exports === 'undefined' ? window : exports));
